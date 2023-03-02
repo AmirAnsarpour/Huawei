fe84fca285748bf5435a4259256cde4ebf9794fb
AmirAnsarpour <amir.ansarpour2003@gmail.com>
2023-03-02 11:59:38 +0330

    slider-update

--- js/index.js
+++ js/index.js
@@ -15,41 +15,7 @@ $(document).ready(function () {
     // $($("#follow-me")).click(function () {
     //     $("#follow-me").addClass("zero-height");
     // });
-    // carousel slider mobile
-    let counter = 1;
-    let size = $(".carousel-slide img")[0].clientWidth;
-    let carouselSlide = $(".carousel-slide");
-    let carouselImages = $(".carousel-slide img");
-    let prevBtn = $("#prevBtn");
-    let nextBtn = $("#nextBtn");
 
-    carouselSlide.css("transform", "translateX(" + (-size * counter) + "px)");
-
-    $(nextBtn).click(function () {
-        if (counter >= carouselImages.length - 1) return;
-        carouselSlide.css("transition", "transform 0.6s ease-in-out")
-        counter++;
-        carouselSlide.css("transform", "translateX(" + (-size * counter) + "px)");
-    });
-
-    $(prevBtn).click(function () {
-        if (counter <= 0) return;
-        carouselSlide.css("transition", "transform 0.6s ease-in-out")
-        counter--;
-        carouselSlide.css("transform", "translateX(" + (-size * counter) + "px)");
-    });
-    carouselSlide.addEventListener("transitionend", function () {
-        if (carouselImages[counter].id === "lastClone") {
-            carouselSlide.style.transition = "none";
-            counter = carouselImages.length - 1;
-            carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-        }
-        if (carouselImages[counter].id === "firstClone") {
-            carouselSlide.style.transition = "none";
-            counter = carouselImages.length - counter;
-            carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-        }
-    })
     // First button "#one-black-navbar"
     // First button icon  "#one-black-navbar i:last-child"
     $($("#one-black-navbar")).click(function () {
